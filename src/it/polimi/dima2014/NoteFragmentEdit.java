24edb9222fb3af30c2747d6da2d37eb00d25d1a0
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2014-12-08 12:10:07 +0100

    Delete the note if the editor fragment is closed with an empty note on deck.

--- src/it/polimi/dima2014/NoteFragmentEdit.java
+++ src/it/polimi/dima2014/NoteFragmentEdit.java
@@ -25,92 +25,101 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class NoteFragmentEdit extends Fragment implements CurrentEventLocationResult {
-	public static final String TAG = "NoteFragmentEdit";
+    public static final String TAG = "NoteFragmentEdit";
 
-	private Note note;
-	private OnNoteEditDoneListener editDoneListener;
+    private Note note;
+    private OnNoteEditDoneListener editDoneListener;
 
-	public interface OnNoteEditDoneListener {
-		public void onSave(Note note);
+    public interface OnNoteEditDoneListener {
+        public void onSave(Note note);
 
-		public void onCancel(long id);
-	}
+        public void onCancel(long id);
+    }
 
-	public NoteFragmentEdit(Note note) {
-		this.note = note;
-	}
+    public NoteFragmentEdit(Note note) {
+        this.note = note;
+    }
 
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
-		try {
-			this.editDoneListener = (OnNoteEditDoneListener) activity;
-		}
-		catch (ClassCastException e) {
-			throw new ClassCastException(activity.toString() + " must implement OnNoteEditDoneListener");
-		}
-	}
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            this.editDoneListener = (OnNoteEditDoneListener) activity;
+        }
+        catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnNoteEditDoneListener");
+        }
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_note_edit, container, false);
-		EditText noteTitle = (EditText) rootView.findViewById(R.id.noteTitleEdit);
-		EditText noteContent = (EditText) rootView.findViewById(R.id.noteContentEdit);
-		noteTitle.setText(this.note.getTitle());
-		noteContent.setText(this.note.getContent());
-		Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
-		cancelButton.setOnClickListener(new OnClickListener() {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_note_edit, container, false);
+        EditText noteTitle = (EditText) rootView.findViewById(R.id.noteTitleEdit);
+        EditText noteContent = (EditText) rootView.findViewById(R.id.noteContentEdit);
+        noteTitle.setText(this.note.getTitle());
+        noteContent.setText(this.note.getContent());
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				if (NoteFragmentEdit.this.note.isFirst()) {
-					NoteFragmentEdit.this.editDoneListener.onCancel(NoteFragmentEdit.this.note.getId());
-				}
-				else {
-					NoteFragmentEdit.this.editDoneListener.onCancel(-1);
-				}
-			}
+            @Override
+            public void onClick(View v) {
+                if (NoteFragmentEdit.this.note.isFirst()) {
+                    NoteFragmentEdit.this.editDoneListener.onCancel(NoteFragmentEdit.this.note.getId());
+                }
+                else {
+                    NoteFragmentEdit.this.editDoneListener.onCancel(-1);
+                }
+            }
 
-		});
-		Button saveButton = (Button) rootView.findViewById(R.id.saveButton);
-		saveButton.setOnClickListener(new OnClickListener() {
+        });
+        Button saveButton = (Button) rootView.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				EditText noteTitle = (EditText) v.getRootView().findViewById(R.id.noteTitleEdit);
-				EditText noteContent = (EditText) v.getRootView().findViewById(R.id.noteContentEdit);
-				ContentValues values = new ContentValues();
-				values.put(NotesOpenHelper.KEY, noteTitle.getText().toString());
-				values.put(NotesOpenHelper.VALUE, noteContent.getText().toString());
-				Uri uri = Uri.parse(NotesContentProvider.CONTENT_URI + "/" + NoteFragmentEdit.this.note.getId());
-				getActivity().getContentResolver().update(uri, values, null, null);
-				Note note = new Note(NoteFragmentEdit.this.note.getId(), new DateTime(), noteTitle.getText().toString(), noteContent.getText().toString());
-				NoteFragmentEdit.this.editDoneListener.onSave(note);
-			}
+            @Override
+            public void onClick(View v) {
+                EditText noteTitle = (EditText) v.getRootView().findViewById(R.id.noteTitleEdit);
+                EditText noteContent = (EditText) v.getRootView().findViewById(R.id.noteContentEdit);
+                ContentValues values = new ContentValues();
+                values.put(NotesOpenHelper.KEY, noteTitle.getText().toString());
+                values.put(NotesOpenHelper.VALUE, noteContent.getText().toString());
+                Uri uri = Uri.parse(NotesContentProvider.CONTENT_URI + "/" + NoteFragmentEdit.this.note.getId());
+                getActivity().getContentResolver().update(uri, values, null, null);
+                Note note = new Note(NoteFragmentEdit.this.note.getId(), new DateTime(), noteTitle.getText().toString(), noteContent.getText().toString());
+                NoteFragmentEdit.this.editDoneListener.onSave(note);
+            }
 
-		});
+        });
 
-		return rootView;
-	}
+        return rootView;
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		EditText title = (EditText) getActivity().findViewById(R.id.noteTitleEdit);
-		if (title.getText().toString().equals("")) {
-			Log.i(MainActivity.TAG, "Launching the calendar task...");
-			Calendar utc = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-			long current = utc.getTimeInMillis();
-			CurrentEventLocationTask task = new CurrentEventLocationTask();
-			task.setDelegate(this);
-			task.setContentResolver(getActivity().getContentResolver());
-			task.execute(current);
-		}
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        EditText title = (EditText) getActivity().findViewById(R.id.noteTitleEdit);
+        if (title.getText().toString().equals("")) {
+            Log.i(MainActivity.TAG, "Launching the calendar task...");
+            Calendar utc = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+            long current = utc.getTimeInMillis();
+            CurrentEventLocationTask task = new CurrentEventLocationTask();
+            task.setDelegate(this);
+            task.setContentResolver(getActivity().getContentResolver());
+            task.execute(current);
+        }
+    }
 
-	@Override
-	public void processResult(String result) {
-		EditText title = (EditText) getActivity().findViewById(R.id.noteTitleEdit);
-		title.setHint("Note @ " + result);
-	}
+    @Override
+    public void onPause() {
+        if (NoteFragmentEdit.this.note.isFirst()) {
+            Uri uri = Uri.parse(NotesContentProvider.CONTENT_URI + "/" + this.note.getId());
+            getActivity().getContentResolver().delete(uri, null, null);
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void processResult(String result) {
+        EditText title = (EditText) getActivity().findViewById(R.id.noteTitleEdit);
+        title.setHint("Note @ " + result);
+    }
 }
